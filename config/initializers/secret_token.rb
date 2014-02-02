6f7bbd2633698864e167f2b662f61651550e9ebf
Jonanfer <jfdobg-18@hotmail.com>
2014-02-02 14:17:39 -0500

    Proyecto de Listas en Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MegaLists::Application.config.secret_token = '6a71a16b80ff5df4a2231c358437fe4defb0cb5b4e0ef6e011bcb8b87b470ac154f5e2e7222dd9757241e0f1781bdc4731175abfc740b043c9da77776fe8b2d8'
